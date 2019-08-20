5deb3d289dc75a50298123b3e188e60d1905c9ae
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-08-20 09:26:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+var URL = "https://api.cloudinary.com/v1_1/dwy9bl7kl/image/upload"
+var UnsignedUploadPreset = "e5xe8bay"
+
+var	xhr = new XMLHttpRequest();
+
+function get_photo() {
+
+	username = document.getElementById("username").value;
+
+	var find_id = 'https://www.instagram.com/' + username + '/?__a=1';
+
+	$.getJSON(find_id, function(json) {
+
+		var user_id = json.graphql.user.id;
+
+		console.log("User ID: " + user_id)
+
+		var photo = json.graphql.user.profile_pic_url_hd; // Profile photo
+
+		console.log("Photo: " + photo);
+
+		document.getElementById("profile").src = photo;
+
+	})
+
+};
+
+
+function server_photos() {
+
+
+	xhr.open("GET", )
+}
+
+function button() {
+	get_photo();
+}
+
+function enter() {
+	if (event.keyCode == 13) {
+		get_photo()
+	}
+}
+
+
+function upload() {
+
+	var file = document.getElementById("profile").src;
+
+	var formData = new FormData();
+	
+
+	formData.append("file", file);
+	formData.append("upload_preset", UnsignedUploadPreset);
+	formData.append("tags", document.getElementById("username").value);
+
+	xhr.open("POST", URL);
+
+	xhr.onload = () => {
+
+   		if (xhr.status === 200) { // Succes
+
+   			var response = JSON.parse(xhr.response);
+   			var server_image = response.secure_url; // https address of uploaded file
+   			console.log("Server Image: " + server_image)
+
+   		} else { // Error
+
+   			alert("Upload failed. Please try agian.");
+   		}
+	}
+
+	xhr.send(formData);
+}
